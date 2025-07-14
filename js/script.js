69ba265a2e2ffbd0164a194a68acdb2f4f682c81
Marco Bacci <marcobacci94@gmail.com>
2025-07-14 14:14:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const bicycles = [
+  {
+    brand : `bianchi`,
+    weight : `8`
+  }
+  {
+    brand : `colnago`,
+    weight : `7.5`
+  }
+  {
+    brand : `canyon`,
+    weight : `7.7`
+  }
+  
+  {
+    brand : `pinarello`,
+    weight : `8.5`
+  }
+  {
+    brand : `trek`,
+    weight : `7.3`
+  }
+]
+
+console.log(bicycles)
