9c40cb127251a5e6b17190d868b87f73fccc1854
Marco Bacci <marcobacci94@gmail.com>
2025-07-14 14:50:24 +0200

    feat: array of teams

--- js/script.js
+++ js/script.js
@@ -1,34 +1,61 @@
 const bicycles = [
   {
-    brand : `bianchi`,
-    weightKg : `8`
+    brand: `bianchi`,
+    weightKg: `8`,
   },
   {
-    brand : `colnago`,
-    weightKg : `7.5`
+    brand: `colnago`,
+    weightKg: `7.5`,
   },
   {
-    brand : `canyon`,
-    weightKg : `7.7`
+    brand: `canyon`,
+    weightKg: `7.7`,
   },
-  
-  {
-    brand : `pinarello`,
-    weightKg : `8.5`
-  },
-  {
-    brand : `trek`,
-    weightKg : `7.3`
-  }
-]
-// determino il primo indice dell'array per compararlo con gli altri
-let lightest = bicycles[0]
-// ciclo l'array per comparare tutti gli elementi
-for(i=0; i<bicycles.length; i++){
-  if(bicycles[i].weightKg < lightest.weightKg ){
-    lightest =  bicycles[i]
 
+  {
+    brand: `pinarello`,
+    weightKg: `8.5`,
+  },
+  {
+    brand: `trek`,
+    weightKg: `7.3`,
+  },
+];
+// determino il primo indice dell'array per compararlo con gli altri
+let lightest = bicycles[0];
+// ciclo l'array per comparare tutti gli elementi
+for (i = 0; i < bicycles.length; i++) {
+  if (bicycles[i].weightKg < lightest.weightKg) {
+    lightest = bicycles[i];
   }
 }
 
-console.log(lightest)
+console.log(lightest);
+
+const team = [
+  {
+    nome: `juventus`,
+    points: 0,
+    faults: 0,
+  },
+  {
+    nome: `napoli`,
+    points: 0,
+    faults: 0,
+  },
+  {
+    nome: `atalanta`,
+    points: 0,
+    faults: 0,
+  },
+  {
+    nome: `inter`,
+    points: 0,
+    faults: 0,
+  },
+  {
+    nome: `roma`,
+    points: 0,
+    faults: 0,
+  },
+];
